0c936ae0f5b5ae8589ddc76917538336c8af8477
Dimitar <Dimitar>
2022-03-29 16:31:36 +0300

    Add pascal triangle

---
+++ Algorithms-Fund/PascalTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBenom(row, col));
+        }
+
+        private static int GetBenom(int row, int col)
+        {
+            if (row <= 1 || col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            return GetBenom(row - 1, col - 1) + GetBenom(row - 1, col);
+        }
+    }
+}
